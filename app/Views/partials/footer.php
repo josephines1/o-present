58c396356dbf9c979c39d65988d67646a25cebba
josephines1 <josephine.bless777@gmail.com>
2024-06-23 22:03:40 +0700

    update footer

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -3,14 +3,14 @@
         <div class="row text-center align-items-center flex-row-reverse">
             <div class="col-lg-auto ms-lg-auto">
                 <ul class="list-inline list-inline-dots mb-0">
-                    <li class="list-inline-item"><a href="https://josephines1.github.io/o-present" target="_blank" class="link-secondary" rel="noopener">Documentation</a></li>
+                    <li class="list-inline-item"><a href="https://github.com/josephines1/o-present?tab=readme-ov-file#o-present" target="_blank" class="link-secondary" rel="noopener">Documentation</a></li>
                 </ul>
             </div>
             <div class="col-12 col-lg-auto mt-3 mt-lg-0">
                 <ul class="list-inline list-inline-dots mb-0">
                     <li class="list-inline-item">
                         Copyright &copy; <?= date('Y') ?>
-                        <a href="https://github.com/josephines1/o-present?tab=readme-ov-file#o-present" class="link-secondary">Josephine</a>.
+                        <a href="https://josephines1.github.io/" target="_blank" class="link-secondary">Josephine</a>.
                     </li>
                 </ul>
             </div>
